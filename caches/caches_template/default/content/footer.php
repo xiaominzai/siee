f8e8ba45f29d39fe14b8494ad8d9c81732ef4218
chenyimin <chenyiminchn@qq.com>
2017-02-03 19:00:38 +0800

    ie7以下浏览器提示问题

---
+++ caches/caches_template/default/content/footer.php
@@ -0,0 +1,45 @@
+<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?>				<div id="bottom-list">
+				<img src="<?php echo IMG_PATH;?>tubiao5.svg" width="28"><span>二级学院:</span>
+				<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"link\" data=\"op=link&tag_md5=709bd381183fd6b73714938e35657259&action=type_list&typeid=53&siteid=%24siteid&order=listorder+DESC&num=10&return=dat\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">修改</a>";}$link_tag = pc_base::load_app_class("link_tag", "link");if (method_exists($link_tag, 'type_list')) {$dat = $link_tag->type_list(array('typeid'=>'53','siteid'=>$siteid,'order'=>'listorder DESC','limit'=>'10',));}?>
+                 <select class="select" onchange="top.location.href = this.options[this.selectedIndex].value;" name="select1" target=_blank>
+			          <option selected="selected" value="">--二级学院--</option>
+			         
+					<?php $n=1;if(is_array($dat)) foreach($dat AS $v) { ?>
+			          <option value="<?php echo $v['url'];?>"><?php echo $v['name'];?></option>
+					<?php $n++;}unset($n); ?>
+			       </select>
+			    <?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
+
+					<img src="<?php echo IMG_PATH;?>tubiao5.svg" width="28" style="margin-left:100px;"><span>职能部门:</span>
+
+				<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"link\" data=\"op=link&tag_md5=391e1c4833be141990c141521da30748&action=type_list&typeid=54&siteid=%24siteid&order=listorder+DESC&num=10&return=dat\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">修改</a>";}$link_tag = pc_base::load_app_class("link_tag", "link");if (method_exists($link_tag, 'type_list')) {$dat = $link_tag->type_list(array('typeid'=>'54','siteid'=>$siteid,'order'=>'listorder DESC','limit'=>'10',));}?>
+                 <select class="select" onchange="top.location.href = this.options[this.selectedIndex].value;" name="select1" target=_blank>
+			          <option selected="selected" value="">--职能部门--</option>
+			         
+						<?php $n=1;if(is_array($dat)) foreach($dat AS $v) { ?>
+			          <option value="<?php echo $v['url'];?>"><?php echo $v['name'];?></option>
+						<?php $n++;}unset($n); ?>
+
+			       </select>
+			       <?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
+
+			       <img src="<?php echo IMG_PATH;?>tubiao5.svg" width="28" style="margin-left:100px"><span>常用链接:</span>
+
+					<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"link\" data=\"op=link&tag_md5=a9619a698b2cb24c21573755ba7b6511&action=type_list&typeid=55&siteid=%24siteid&order=listorder+DESC&num=10&return=dat\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">修改</a>";}$link_tag = pc_base::load_app_class("link_tag", "link");if (method_exists($link_tag, 'type_list')) {$dat = $link_tag->type_list(array('typeid'=>'55','siteid'=>$siteid,'order'=>'listorder DESC','limit'=>'10',));}?>
+		            <select class="select" onchange="location.href = this.options[this.selectedIndex].value;" name="select2">
+					          <option selected="selected" value="">--友情链接--</option>
+					          <?php $n=1;if(is_array($dat)) foreach($dat AS $v) { ?>
+					          <option value="<?php echo $v['url'];?>" target=_blank><?php echo $v['name'];?></option>
+					          <?php $n++;}unset($n); ?>
+					</select>
+					<?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
+				</div>
+				<div id="bottom">
+					<span>Copyright © 2016 华南理工大学广州学院创新创业教育学院版权所有</span>
+					<span>技术支持：学生传媒中心前端技术部&nbsp;&nbsp;学校地址:&nbsp;广州市花都区新华街学府路一号&nbsp;&nbsp;&nbsp;联系方式:020-36907036　邮编：510800</span>
+				</div>
+			</div>
+	</div>
+	<script language="JavaScript" src="<?php echo APP_PATH;?>api.php?op=count&id=<?php echo $id;?>&modelid=<?php echo $modelid;?>"></script> 
+</body>
+</html>
---
+++ caches/caches_template/default/content/header.php
@@ -0,0 +1,102 @@
+<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH;?>style.css">
+    <script src="//cdn.bootcss.com/jquery/1.8.3/jquery.min.js"></script>
+    <script src="<?php echo JS_PATH;?>common.js" type="text/javascript"></script>
+    <script src="<?php echo JS_PATH;?>unslider.min.js"></script>
+    <script src="<?php echo JS_PATH;?>search_common.js"></script>
+    <link rel="icon" href="<?php echo IMG_PATH;?>huaguang.ico" type="image/x-icon"/>
+    
+    <link rel="stylesheet" href="<?php echo CSS_PATH;?>min.css">
+
+    <!--iealter-->
+    <script src="<?php echo JS_PATH;?>iealert.js"></script>
+    <link rel="stylesheet" href="<?php echo CSS_PATH;?>iealert/style.css">
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("body").iealert();
+        });
+    </script>
+
+    <script type="text/javascript">
+
+            function detectBrowser()
+            {
+            var browser=navigator.appName
+            var b_version=navigator.appVersion
+            var version=parseFloat(b_version)
+            
+            if ((browser=="Netscape"||browser=="Microsoft Internet Explorer")
+            && (version>=5))
+              {document.getElementById('detectBrowser').style.display="none"}
+            else
+              {document.getElementById('detectBrowser').style.display="block";}
+              }
+    </script>
+    <title>华南理工大学广州学院创新创业教育学院</title>
+</head>
+<body onload="detectBrowser()">
+ <div id="detectBrowser" class="center" style="display: none;">您的浏览器版本过低，为了您更好的体验，请更换浏览器</div>
+    <div id="main">
+        <div id="imgfloat">
+            <img src="<?php echo IMG_PATH;?>index-icon.jpg" width="96px" alt="" >
+            <div style="font-weight: bold;margin-top: 5px;text-align: center;">
+                华广双创公众号
+            </div>
+        </div>
+        <div id="header">
+            <div class="header-up">
+                <img src="<?php echo IMG_PATH;?>headerup.svg"  width="601" height="92" />
+                <form id="sousuo" method="get" target="_blank" action="<?php echo APP_PATH;?>index.php">
+                    <div id="try">
+                        
+                        <input type="hidden" name="m" value="search"/>
+                        <input type="hidden" name="c" value="index"/>
+                        <input type="hidden" name="a" value="init"/>
+                        <input type="hidden" name="typeid" value="1" id="typeid"/>
+                        <input type="hidden" name="siteid" value="<?php echo $siteid;?>" id="siteid"/>
+                        <input type="text" name="q" id="sousuo-text" placeholder="关键字搜索">
+                        <input type="submit" id="sousuo-button" value=""/>
+                        
+                    </div>
+                </from>
+            </div>
+            <!-- 导航栏部分 -->
+            <div id="nav">
+                <ul id="navli" class="clearfix">
+                    <li ><a  class="ban" id="one" href="<?php echo APP_PATH;?>">首页</a></li>
+                    <li ><a  class="ban" href="#">学院概括</a>
+                        <ul id="nav1" class="nav">
+                            <li>
+                                <div class="nav_div"><div class="nav_two"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=20">学院简介</a></div></div>
+                            </li>
+                            <li><div class="nav_div"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=21">组织架构</a></div></li>
+                            <li id="teacher"><div class="nav_div"><a href="#" class="li_bottom" id="double_li">双创导师</div></a>
+                                    <ul id="nav2" class="nav2">
+                                        <li><div class="nav_div"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=19">校内导师</a></div></li>
+                                        <li><div class="nav_div"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=18" class="li_bottom">校外导师</a></div></li>
+                                    </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li ><a  class="ban" href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=10">通知公告</a></li>
+                    <li ><a  class="ban" href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=11">双创动态</a>
+                        <ul id="nav3" class="nav">
+                            <li><div class="nav_div"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=22">讲座沙龙</a></div></li>
+                            <li><div class="nav_div"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=23">创业竞赛</a></div></li>
+                            <li><div class="nav_div"><a class="li_bottom" href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=24" >其他</a></div></li>
+                        </ul>
+                    </li>
+                    <li ><a  class="ban" href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=12">政策服务</a></li>
+                    <li ><a  class="ban" href="#">创业基地</a>
+                        <ul id="nav4" class="nav">
+                            <li><div class="nav_div"><a href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=25">校外基地</a></div></li>
+                            <li><div class="nav_div"><a class="li_bottom" href="<?php echo APP_PATH;?>index.php?m=content&c=index&a=lists&catid=26">创业园区</a></div></li>
+                        </ul>
+
+                    </li>
+
+                </ul>
+            </div>
