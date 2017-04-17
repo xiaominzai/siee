d48ea5ed55190e37a11a460f3aadaac411b1a7bf
陈逸民 <chenyiminchn@qq.com>
2017-04-17 22:33:29 +0800

    phpcms打补丁

--- phpcms/modules/content/fields/image/form.inc.php
+++ phpcms/modules/content/fields/image/form.inc.php
@@ -1,25 +1,29 @@
-	function image($field, $value, $fieldinfo) {
-		$setting = string2array($fieldinfo['setting']);
-		extract($setting);
+	function images($field, $value, $fieldinfo) {
+		extract($fieldinfo);
+		$list_str = '';
+		if($value) {
+			$value = string2array(new_html_entity_decode($value));
+			if(is_array($value)) {
+				foreach($value as $_k=>$_v) {
+				$list_str .= "<div id='image_{$field}_{$_k}' style='padding:1px'><input type='text' name='{$field}_url[]' value='{$_v[url]}' style='width:310px;' ondblclick='image_priview(this.value);' class='input-text'> <input type='text' name='{$field}_alt[]' value='{$_v[alt]}' style='width:160px;' class='input-text'> <a href=\"javascript:remove_div('image_{$field}_{$_k}')\">".L('remove_out', '', 'content')."</a></div>";
+				}
+			}
+		} else {
+			$list_str .= "<center><div class='onShow' id='nameTip'>".L('upload_pic_max', '', 'content')." <font color='red'>{$upload_number}</font> ".L('tips_pics', '', 'content')."</div></center>";
+		}
+		$string = '<input name="info['.$field.']" type="hidden" value="1">
+		<fieldset class="blue pad-10">
+        <legend>'.L('pic_list').'</legend>';
+		$string .= $list_str;
+		$string .= '<div id="'.$field.'" class="picList"></div>
+		</fieldset>
+		<div class="bk10"></div>
+		';
 		if(!defined('IMAGES_INIT')) {
-			$str = '<script type="text/javascript" src="'.JS_PATH.'swfupload/swf2ckeditor.js"></script>';
+			$str = '<script type="text/javascript" src="statics/js/swfupload/swf2ckeditor.js"></script>';
 			define('IMAGES_INIT', 1);
 		}
-		$html = '';
-		if (defined('IN_ADMIN')) {
-			$html = "<input type=\"button\" style=\"width: 66px;\" class=\"button\" onclick=\"crop_cut_".$field."($('#$field').val());return false;\" value=\"".L('cut_the_picture','','content')."\"><input type=\"button\" style=\"width: 66px;\" class=\"button\" onclick=\"$('#".$field."_preview').attr('src','".IMG_PATH."icon/upload-pic.png');$('#".$field."').val(' ');return false;\" value=\"".L('cancel_the_picture','','content')."\"><script type=\"text/javascript\">function crop_cut_".$field."(id){
-	if (id=='') { alert('".L('upload_thumbnails', '', 'content')."');return false;}
-	window.top.art.dialog({title:'".L('cut_the_picture','','content')."', id:'crop', iframe:'index.php?m=content&c=content&a=public_crop&module=content&catid='+catid+'&picurl='+encodeURIComponent(id)+'&input=$field&preview=".($show_type && defined('IN_ADMIN') ? $field."_preview" : '')."', width:'680px', height:'480px'}, 	function(){var d = window.top.art.dialog({id:'crop'}).data.iframe;
-	d.uploadfile();return false;}, function(){window.top.art.dialog({id:'crop'}).close()});
-};</script>";
-			}
-		$authkey = upload_key("1,$upload_allowext,$isselectimage,$images_width,$images_height,$watermark");
-		if($show_type && defined('IN_ADMIN')) {
-			$preview_img = $value ? $value : IMG_PATH.'icon/upload-pic.png';
-			return $str."<div class='upload-pic img-wrap'><input type='hidden' name='info[$field]' id='$field' value='$value'>
-			<a href='javascript:void(0);' onclick=\"flashupload('{$field}_images', '".L('attachment_upload', '', 'content')."','{$field}',thumb_images,'1,{$upload_allowext},$isselectimage,$images_width,$images_height,$watermark','content','$this->catid','$authkey');return false;\">
-			<img src='$preview_img' id='{$field}_preview' width='135' height='113' style='cursor:hand' /></a>".$html."</div>";
-		} else {
-			return $str."<input type='text' name='info[$field]' id='$field' value='$value' size='$size' class='input-text' />  <input type='button' class='button' onclick=\"flashupload('{$field}_images', '".L('attachment_upload', '', 'content')."','{$field}',submit_images,'1,{$upload_allowext},$isselectimage,$images_width,$images_height,$watermark','content','$this->catid','$authkey')\"/ value='".L('upload_pic', '', 'content')."'>".$html;
-		}
+		$authkey = upload_key("$upload_number,$upload_allowext,$isselectimage");
+		$string .= $str."<div class='picBut cu'><a href='javascript:void(0);' onclick=\"javascript:flashupload('{$field}_images', '".L('attachment_upload')."','{$field}',change_images,'{$upload_number},{$upload_allowext},{$isselectimage}','content','$this->catid','{$authkey}')\"/> ".L('select_pic')." </a></div>";
+		return $string;
 	}
